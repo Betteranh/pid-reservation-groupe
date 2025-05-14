393286b42f733a21547c963b71aee12b99a03eac
Betteranh <danguyen.1129@gmail.com>
2025-05-14 18:31:30 +0200

    MAJ SHOW_TAG

--- src/main/resources/db/migration/V18_1__create_show_tag_table.sql
+++ src/main/resources/db/migration/V18_1__create_show_tag_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE show_tag
 (
-    show_id BIGINT NOT NULL,
-    tag_id  BIGINT NOT NULL,
+    show_id INT(11) NOT NULL,
+    tag_id  INT(11) NOT NULL,
     PRIMARY KEY (show_id, tag_id),
     CONSTRAINT fk_show FOREIGN KEY (show_id)
         REFERENCES shows (id)
@@ -11,4 +11,6 @@ CREATE TABLE show_tag
         REFERENCES tags (id)
         ON UPDATE CASCADE
         ON DELETE RESTRICT
-);
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
